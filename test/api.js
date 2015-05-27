f217e846057e53971482f93341a4fd40115eec0a
Daniel Cousens <github@dcousens.com>
2015-05-27 14:29:36 +1000

    fix api tests

--- test/api.js
+++ test/api.js
@@ -8,10 +8,12 @@ test('api usage', function (t) {
   happiness.lintFiles([], { cwd: 'bin' }, function (err, result) {
     t.error(err, 'no error while linting')
     t.equal(typeof result, 'object', 'result is an object')
-    t.equal(result.errorCount, 42, 'error count 42')
+    t.equal(result.errorCount, 61, 'error count 61')
 
     t.equal(result.results[0].filePath, filePath, 'error filepath correct')
-    t.equal(result.results[0].messages[0].message, 'Missing semicolon.', 'first mising semicolon message')
-    t.equal(result.results[0].messages[0].message, 'Missing semicolon.', 'second mising semicolon message')
+
+    var errors = result.results[0].messages
+    t.equal(errors[0].message, 'Missing semicolon.', 'first mising semicolon message')
+    t.equal(errors[1].message, 'Missing semicolon.', 'second mising semicolon message')
   })
 })
